6ef22b8f2e68aaf9a3487e7b698884b3566f9efb
japiirainen <joona.piirainen@gmail.com>
2025-01-01 14:49:51 +0200

    feat(ideal): read input from file or stdin

--- ideal/src/main.rs
+++ ideal/src/main.rs
@@ -1,6 +1,10 @@
 #[macro_use]
 mod cli;
 
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::Path;
+
 use crate::cli::{Commands, Options};
 
 use clap::Parser;
@@ -10,7 +14,38 @@ pub fn main() {
 
     match options.command {
         Commands::Interpret { filepath } => {
-            println!("interpret: {}", filepath);
+            let input = get_input(filepath);
+            match input {
+                Ok(input) => interpret(input),
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+    }
+}
+
+fn interpret(input: String) {
+    println!("{}", input);
+}
+
+fn get_input(filepath: String) -> Result<String, String> {
+    match filepath.as_str() {
+        "-" => {
+            let mut buffer = String::new();
+            std::io::stdin()
+                .read_to_string(&mut buffer)
+                .expect("Failed to read from stdin");
+            Ok(buffer)
+        }
+        _ => {
+            let filename = Path::new(&filepath);
+            let file = File::open(filename)
+                .map_err(|_| format!("Failed to open file: {}", filename.display()))?;
+            let mut reader = BufReader::new(file);
+            let mut contents = String::new();
+            reader
+                .read_to_string(&mut contents)
+                .map_err(|_| format!("Failed to read from file: {}", filename.display()))?;
+            Ok(contents)
         }
     }
 }
